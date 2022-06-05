fd825888e72e77960537ed5efea7875c4e0ae547
Shivaji Thattikota <shivajithattikota@outlook.com>
2022-06-05 10:33:10 +0530

    Child processes - exec, exec_file, spawn

---
+++ 002_child_processes/spawn_demo.js
@@ -0,0 +1,24 @@
+const { spawn } = require('child_process');
+
+const child = spawn('ls', ['-lh']);
+
+child.stdout.on('data', (data) => {
+    console.log('stdout: ', data)
+})
+
+child.stderr.on('data', (data) => {
+    console.log('stderr: ', data)
+})
+
+child.on('error', (error) => {
+    console.log('error: ', error.message)
+})
+
+child.on('exit', (code, signal) => {
+    if (code) {
+        console.log("Process exited with code: ", code)
+    }
+    if (signal) {
+        console.log("Process killed with signal: ", signal)
+    }
+})
